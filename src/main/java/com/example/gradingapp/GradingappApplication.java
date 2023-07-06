3f14ddbba53806b71f363b7bf68f960132c6a1d0
LazarevicAleksandra <l.aleksandra1997@gmail.com>
2023-07-06 15:33:13 +0200

    init commit

---
+++ src/main/java/com/example/gradingapp/GradingappApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradingapp/GradingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
